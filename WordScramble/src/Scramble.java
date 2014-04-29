90c6696541b945de459f8d9789542ca6f957bd68
RGibbons <ryanagibbs1@gmail.com>
2014-04-29 02:40:02 -0400

    Scrambling Code is added
    This code will be the workhorse of the application. I built it in
    eclipse, independent of an Android design, so the code will have to be
    refactored slightly in order to work in the app. Regardless, you should
    be able to download it and use it to test it out. Remember, the SRC
    contains the java code and the BIN contains the compiled java bytecode

---
+++ WordScramble/src/Scramble.java
@@ -0,0 +1,104 @@
+import java.util.*;
+
+
+public class Scramble {
+
+	public static void main(String[] args) {
+		
+		int randInt;
+		String first;
+		String middle = "";
+		String last;
+		String word = "";
+		String used = "";
+		Random rand = new Random();
+		
+		//Takes a input stream - this will be different in the android app
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter the String that you want to be scrambled: ");
+		
+		//This string has the entire string that was entered
+		String scrambleString = scanner.nextLine();
+		
+		//This scanner is used to break down the string into tokens, or words in this case
+		Scanner tokenizer = new Scanner(scrambleString);
+		String finalString = "";
+		
+		//String builders are used to add to as the new scrambled string is constructed.
+		StringBuilder stringbuilder = new StringBuilder();
+		//This StringBuilder is used to ensure that the random letters chosen are not the same letter.
+		//Future problem to be dealth with - how to deal with words longer than 13 letters?
+		StringBuilder usedbuilder = new StringBuilder();
+		
+		
+		//While there are more words
+		while(tokenizer.hasNext()){
+			
+			//get word
+			word = tokenizer.next();
+			
+			
+			//if word is longer than 3 characters - this is because it is impossible to scramble a word of less than
+			//3 characters while keeping the first and last letters intact
+			if(word.length() > 3){
+				
+				
+				first = word.substring(0,1);		//holds first letter of word
+				middle = word.substring(1, word.length()-1); 	//holds middle part that will be scrambled
+				last = word.substring((word.length()-1), word.length());	//holds last letter of the word
+				
+				
+				stringbuilder.append(first);
+				
+				//add as many characters to the string builder that exist in the string "middle"
+				for(int i = 0; i < middle.length(); i++){
+					
+					//create a random integer that has a valid index for middle
+					randInt = rand.nextInt(middle.length());
+					
+					//makes sure randInt hasn't already been added
+					while((usedbuilder.indexOf((String.valueOf(randInt))))>=0){
+						
+						randInt = rand.nextInt(middle.length());
+						
+						
+					}
+					
+					
+					usedbuilder.append(randInt);
+					String temp = "";
+					temp = middle.substring(randInt,randInt+1);	//add one of the letters
+					stringbuilder.append(temp);
+					
+					
+				}
+				
+				//after all of the middle letters have been added back, add the last letter and a space 
+				//and start the process over.
+				stringbuilder.append(last);
+				stringbuilder.append(" ");
+				
+				//clear usedbuilder so that a new set of integers can be tracked.
+			    usedbuilder.delete(0, usedbuilder.length());
+				
+			}
+			
+			//If the word is 3 characters or fewer, simply append the word as it is.
+			else{
+			
+				stringbuilder.append(word + " ");
+				
+			}
+		
+		}
+		
+		
+		//FINALLY, build the string and then output it to the user. Once again, this will be different slightly
+		//in the actual android app.
+		finalString = stringbuilder.toString();
+		System.out.println(finalString);
+		
+
+	}
+
+}
